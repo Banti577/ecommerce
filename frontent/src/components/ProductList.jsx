ff1590cac6cc01b690c9ad0bda6677b4654420d8
Banti Patel <93613410+Banti577@users.noreply.github.com>
2026-03-02 16:01:49 +0530

    Added Lazy Loading For Images

--- frontent/src/components/ProductList.jsx
+++ frontent/src/components/ProductList.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useTheme } from "../contexts/ThemeContext";
 import { useMemo, useState } from "react";
 
+import LazyImage from "./LazyImage";
+
 const ProductList = ({ products }) => {
   const { theme } = useTheme();
   const [selectedCategory, setSelectedCategory] = useState("all");
@@ -41,41 +43,32 @@ const ProductList = ({ products }) => {
         <div className="w-full flex flex-wrap justify-center gap-6 p-1">
           {filteredProducts.map((item) => (
             <div
-              key={item._id}
-              className={`${theme.curr_BG} ${theme.curr_TEXT}
-            w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/5
-            rounded-xl border border-gray-200
-            shadow-sm hover:shadow-lg transition-all duration-300 overflow-hidden`}
+              className={`${theme.curr_BG} ${theme.curr_TEXT} w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/5 rounded-xl border border-gray-200 shadow-sm hover:shadow-lg transition-all duration-300 overflow-hidden`}
             >
               <Link to={`/products/${item._id}`} className="block">
-                <img
-                  className="w-full h-48 object-cover"
+                <LazyImage
                   src={item.productImg}
                   alt={item.productName}
+                  className="w-full h-48"
                 />
 
                 <div className="p-4">
                   <h1 className="text-base font-semibold truncate mb-1">
                     {item.productName}
                   </h1>
-
                   <p className="text-sm opacity-80 line-clamp-2 mb-3">
                     {item.productDesc}
                   </p>
-
                   <p className="text-xs font-medium opacity-60 mb-2">
                     Category: {item.category}
                   </p>
-
                   <div className="flex items-center gap-2">
                     <span className="text-lg font-bold text-green-600 dark:text-green-400">
                       ₹{item.offerPrice}
                     </span>
-
                     <span className="text-sm opacity-60 line-through">
                       ₹{item.productPrice}
                     </span>
-
                     <span className="text-sm font-medium text-red-600 dark:text-red-400">
                       {item.productDiscount}% off
                     </span>
