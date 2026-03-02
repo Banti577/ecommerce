85ef359ecb13ea726873920ebe384f31dcc9d9ee
Banti Patel <93613410+Banti577@users.noreply.github.com>
2026-03-02 16:03:43 +0530

    resolved user model issue

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema(
             unique: true,
             lowercase: true,
             trim: true,
-            lowercase: true
         },
         gender: {
             type: String,
